ac87c4fac33a2f8d551c9ddb417e3f092a17a3ff
adalarugur <adalarugur@gmail.com>
2022-01-23 22:55:47 +0300

    socket

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app  = require("express")();
 const server = require("http").createServer(app);  
 const cors = require("cors");
+const { Socket } = require("dgram");
 
 const io = require("socket.io")(server,{
     cors: {
@@ -14,7 +15,22 @@ app.use(cors());
 const PORT = process.env.PORT  || 5000;
 
 app.get("/",(req,res)=>{
-    res.send("server run");
+    res.send("server run"); 
 }); 
+
+io.on('connection',(socket)=>{
+    console.log("someone connected")
+    socket.emit('me', socket.id);
+
+    socket.on('disconnet',()=>{
+        socket.broadcast.emit("callended");
+        
+    });
+
+    socket.on("calluser",({userToCall, signalData,from,name})=>{
+        
+    });
+
+});
 
 server.listen(PORT,() => console.log("server listen port ${'PORT'}"));
