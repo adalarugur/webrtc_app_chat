794e3d3b1d429131ac2dac7b27b50ec0d30a4ec3
adalarugur <adalarugur@gmail.com>
2022-01-24 23:35:55 +0300

    context completed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import { ContextProvider } from './Context';
 import './styles.css';
 
 ReactDOM.render(<App/>, document.getElementById('root' ));
